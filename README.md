39fad74575ea7bb28a4c97f4371fcfacdd69677b
srinumanikanta katha <srinumanikanta0011@gmail.com>
2024-02-24 23:35:47 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 "# dropdown_react" 
+
+
+## functionalities 
+
+   ---Itemclickfunction
+
+## hooks
+   ---usestate Hook
+
+## Events
+
+  ---onMouseLeave
+  ---onMouseEnter
+
+ 
+
+
+
+"# dropdown_react_skill" 
--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -6,10 +6,7 @@ function Dropdown(){
     const [open ,setOpen]=useState(false);
     const items= ["Cars","Bikes","Truck","Auto","Others"];
 
-    // function click(){
-    //     Setopen(!open);
-    // }
-
+    //  After click the item click function hide the list
     const itemclick=(item)=>{
         setOpen(false);
         console.log(item);
@@ -18,17 +15,20 @@ function Dropdown(){
     return(
         <>
          
-            <h1 className="heading">Should You Use a Dropdown ?</h1>
-                <div className="dropdown"  onMouseEnter={() => setOpen(true)}
-        onMouseLeave={() => setOpen(false)}>
+                <h1 className="heading">Should You Use a Dropdown ?</h1>
+            <div className="dropdown"  
+                onMouseEnter={() => setOpen(true)}
+                onMouseLeave={() => setOpen(false)}>
+
                     <button className="button1">Dropdown
-                    {open?  <img className="imagedp" src="https://cdn-icons-png.flaticon.com/128/2961/2961937.png"/>:<img className="imagedp2" src="https://cdn-icons-png.flaticon.com/128/11419/11419206.png"/>}  
+                     {open?  <img className="imagedp" src="https://cdn-icons-png.flaticon.com/128/2961/2961937.png"/>:<img className="imagedp2" src="https://cdn-icons-png.flaticon.com/128/11419/11419206.png"/>}  
                     </button> 
+
                 {open &&<ul className="dplist">
                     {items.map((item)=><li className="dpitem" onClick={()=>itemclick(item)}>{item}</li>)} 
                 </ul>}
                 
-                </div>
+            </div>
          
 
 
